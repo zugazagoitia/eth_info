62ed8671cb387210b5d9bcd016c829143456cecb
Alberto Rodríguez Zugazagoitia <ItzZuga@users.noreply.github.com>
2017-07-26 22:36:38 +0100

    Add files via upload

---
+++ eth_info.js
@@ -0,0 +1,52 @@
+﻿registerPlugin({
+    name: 'Ethereum Price Bot',
+    version: '1.0',
+    description: 'Retrieves ETH price info in EUR and sets it as the name of a channel',
+    author: 'ItzZuga',
+    vars: {
+        channel: {
+            title: 'Chanel',
+            type: 'channel'
+        },
+        interval: {
+            title: 'Interval for the price to be updated update (In minutes)',
+            type: 'number'
+        }
+    }
+}, function(sinusbot, config) {
+
+
+    var interval = config.interval * 60 * 1000;
+
+
+
+
+    // Bitcoin Channel
+    setInterval(function() {
+
+
+        // API de GDAX para ETH-EUR
+        http({
+            url: 'https://api.gdax.com/products/ETH-EUR/ticker'
+        }, function(err, res) {
+            if (err) {
+                log('phpCode: ' + err);
+                return
+            }
+
+            if (res.statusCode == 200) {
+
+
+                var responsee = JSON.parse(res.data);
+
+
+                sinusbot.channelUpdate(config.channel, {
+                    name: '[cspacer]1 ETH = ' + responsee.price + ' EUR'
+                });
+            }
+        });
+
+
+    }, interval);
+
+});
